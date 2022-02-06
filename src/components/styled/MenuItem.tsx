a8b05fed8659015aee54b2f8bdae34a29793c544
almightyblob <dominik.antunovic@gmail.com>
2022-02-06 02:10:34 +0100

    refactored MenuItem

--- src/components/styled/MenuItem.tsx
+++ src/components/styled/MenuItem.tsx
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 interface activeProps {
   active: boolean
 }
-
-const defaultClass = 'text-sm text-zink-600 font-semibold rounded-md hover:bg-zinc-400 p-2';
-const activeClass = 'text-sm text-white bg-zinc-600 font-semibold rounded-md hover:bg-zinc-300 p-2';;
+const baseClasses = 'text-sm font-semibold rounded-md duration-100 p-2'
+const defaultClass = `${baseClasses} text-zink-600 hover:bg-zinc-400`;
+const activeClass = `${baseClasses} text-white bg-zinc-600 hover:bg-zinc-300`;
 
 const MenuItem = styled.button.attrs((props:activeProps) => props.active ? ({className: `${activeClass}`}) : ({className: `${defaultClass}`})  )<activeProps>``;
 
